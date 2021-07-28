13c380522fa9218e664e0f371634a0566e2eaec6
zhangliyu <zhangliyu@koderover.com>
2021-07-28 13:57:32 +0800

    fix bug
    Signed-off-by: zhangliyu <zhangliyu@koderover.com>

--- src/components/projects/env/env_detail/env_detail_comp.vue
+++ src/components/projects/env/env_detail/env_detail_comp.vue
@@ -1005,10 +1005,10 @@ export default {
                 type: 'success'
               })
             }).catch(error => {
-            const description = error.data.description
+            const description = error.response.data.description
             const res = description.match('the following services are modified since last update')
             if (res) {
-              this.updateEnv(error.data.description, product_info)
+              this.updateEnv(description, product_info)
             }
           })
         })
