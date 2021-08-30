4cce9f2b64db563317bd7771dce4dca058ab25ac
leozhang2018 <leozhang2018@gmail.com>
2021-08-30 15:15:47 +0800

    Optimize file loading api.
    Signed-off-by: leozhang2018 <leozhang2018@gmail.com>

--- src/components/projects/web_code/code.vue
+++ src/components/projects/web_code/code.vue
@@ -46,7 +46,7 @@
           class="folder"
           ref="folder"
           :changeModalStatus="changeModalStatus"
-          :laodData="laodData"
+          :loadData="loadData"
           :saveFileName="saveFileName"
           :saveNewFile="saveNewFile"
           :saveNewFolder="saveNewFolder"
@@ -171,14 +171,13 @@ export default {
     closeSelectRepo () {
       this.$refs.repo.closeSelectRepo()
     },
-    laodData (data) {
+    loadData (data) {
+      console.log(typeof data.parent)
       let path = ''
-      if (data.parent) {
-        if (data.parent === '/') {
-          path = data.parent + data.label
-        } else {
-          path = data.parent + '/' + data.label
-        }
+      if (typeof data.parent !== 'undefined') {
+        path = data.parent + '/' + data.label
+      } else {
+        path = ''
       }
       this.expandKey = []
       const params = {
