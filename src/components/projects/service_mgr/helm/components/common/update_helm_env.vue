54d92683a5e91b5dc947a86c3adebe05c577d71f
Jody <qizhaodi@koderover.com>
2021-08-31 14:59:15 +0800

    update helm env
    Signed-off-by: Jody <qizhaodi@koderover.com>

--- src/components/projects/service_mgr/helm/components/common/update_helm_env.vue
+++ src/components/projects/service_mgr/helm/components/common/update_helm_env.vue
@@ -1,28 +1,18 @@
 <template>
-  <el-dialog
-    title="是否更新对应环境？"
-    :visible.sync="updateHelmEnvDialogVisible"
-    width="40%"
-  >
+  <el-dialog title="是否更新对应环境？" :visible.sync="updateHelmEnvDialogVisible" width="40%">
     <div class="title">
-      <el-alert
-        title="勾选需要更新的环境，点击确定之后，该服务将自动在对应的环境中进行更新"
-        :closable="false"
-        type="warning"
-      >
-      </el-alert>
+      <el-alert title="勾选需要更新的环境，点击确定之后，该服务将自动在对应的环境中进行更新" :closable="false" type="warning"></el-alert>
+    </div>
+    <div class="content">
       <el-checkbox-group v-model="checkedEnvList">
-        <el-checkbox
-          v-for="(env, index) in envNameList"
-          :key="index"
-          :label="env.envName"
-        ></el-checkbox>
+        <el-checkbox v-for="(env, index) in envNameList" :key="index" :label="env.envName"></el-checkbox>
       </el-checkbox-group>
+      <div class="tip">
+        <el-switch v-model="readTip" active-text="确认更新 Chart 保存后，即将要更新的相对应环境的 values.yaml 文件和变量已更新！"></el-switch>
+      </div>
     </div>
     <span slot="footer" class="dialog-footer">
-      <el-button size="small" :disabled="!checkedEnvList.length" type="primary" @click="autoUpgradeEnv"
-        >确 定</el-button
-      >
+      <el-button size="small" :disabled="!readTip || !checkedEnvList.length" type="primary" @click="autoUpgradeEnv">确 定</el-button>
       <el-button size="small" @click="skipUpdate">跳过</el-button>
     </span>
   </el-dialog>
@@ -40,7 +30,8 @@ export default {
   },
   data () {
     return {
-      checkedEnvList: []
+      checkedEnvList: [],
+      readTip: false
     }
   },
   methods: {
@@ -104,3 +95,30 @@ export default {
   }
 }
 </script>
+
+<style lang="less" scoped>
+/deep/.el-dialog {
+  .el-dialog__body {
+    padding-top: 10px;
+
+    .title {
+      margin-bottom: 20px;
+    }
+
+    .content {
+      .tip {
+        margin-top: 30px;
+        vertical-align: text-top;
+
+        .el-switch__label {
+          color: #606266;
+
+          &.is-active {
+            color: #409eff;
+          }
+        }
+      }
+    }
+  }
+}
+</style>
