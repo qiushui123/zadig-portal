06c41a3d890bf01658e6dd6bca508b7bdfad674b
qiushui123 <1548110942@qq.com>
2021-09-09 18:48:29 +0800

    edit api
    Signed-off-by: qiushui123 <1548110942@qq.com>

--- src/api/index.js
+++ src/api/index.js
@@ -249,7 +249,7 @@ export function envRevisionsAPI (projectName, envName) {
 
 export function productServicesAPI (projectName, envName, envSource, searchName = '', perPage = 20, page = 1) {
   if (envSource === 'helm' || envSource === 'external') {
-    return http.get(`/api/aslan/environment/environments/${projectName}/groups/${envSource}?envName=${envName}&workloadName=${searchName}&perPage=${perPage}&page=${page}`)
+    return http.get(`/api/aslan/environment/environments/${projectName}/workloads?envName=${envName}&workloadName=${searchName}&perPage=${perPage}&page=${page}`)
   } else {
     return http.get(`/api/aslan/environment/environments/${projectName}/groups?envName=${envName}&serviceName=${searchName}&perPage=${perPage}&page=${page}`)
   }
