5db574f883b644448ab52e2ca51f59519edce1f5
qiushui123 <1548110942@qq.com>
2021-09-26 17:50:44 +0800

    edit service description
    Signed-off-by: qiushui123 <1548110942@qq.com>

--- src/components/common/deploy_icons.vue
+++ src/components/common/deploy_icons.vue
@@ -2,7 +2,13 @@
   <el-popover placement="right"
               trigger="hover">
     <div>
-      <i class="iconfont iconrongqifuwu"></i> {{msg}}
+      <i class="iconfont iconrongqifuwu"></i> 容器服务
+    </div>
+    <div>
+      <i class="iconfont iconhelmrepo"></i> Helm 服务
+    </div>
+    <div>
+      <i class="iconfont iconwuliji"></i> 主机服务
     </div>
     <i class="el-icon-question"
        slot="reference"></i>
@@ -10,30 +16,10 @@
 </template>
 
 <script>
-import { getSingleProjectAPI } from '@/api'
-
 export default {
   data () {
     return {
-      msg: '容器服务'
     }
-  },
-  methods: {
-    async checkProjectFeature () {
-      const projectName = this.$route.params.project_name
-      const projectInfo = await getSingleProjectAPI(projectName)
-      if (projectInfo.product_feature) {
-        const productFeature = projectInfo.product_feature
-        if (productFeature.deploy_type === 'k8s') {
-          if (productFeature.basic_facility === 'cloud_host') {
-            this.msg = '主机服务'
-          }
-        }
-      }
-    }
-  },
-  created () {
-    this.checkProjectFeature()
   }
 }
 </script>
