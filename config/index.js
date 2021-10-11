1378f49f036c6f15471fabbad79cfb7bd4858f03
qiushui123 <1548110942@qq.com>
2021-10-11 21:16:14 +0800

    delete config
    Signed-off-by: qiushui123 <1548110942@qq.com>

--- config/index.js
+++ config/index.js
@@ -3,7 +3,7 @@
 // see http://vuejs-templates.github.io/webpack for documentation.
 
 const path = require('path');
-const backEndAddr = 'http://dev2.test.8slan.com';
+const backEndAddr = '';
 
 module.exports = {
   dev: {
